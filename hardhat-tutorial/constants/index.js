39543a906bb7112f5b24e8963532840cd9b54465
CodingCas <casweeno2000@gmail.com>
2022-04-11 16:33:37 +0100

    Modification and redeployed for Opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x9e92DE115F6c5a66c77062434Fa4F787Fd32daa9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-lovat-tau.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
